40e5388560d7792d5fe06efa0469cb7396e1a3fb
canalreeds <dean.brianr@gmail.com>
2018-09-29 05:02:48 -0400

    Initial upload.

---
+++ Glimpse/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Glimpse
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.resizeButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.picPanel = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.picPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.openButton.Location = new System.Drawing.Point(281, 333);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(68, 25);
+            this.openButton.TabIndex = 1;
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.BackColor = System.Drawing.Color.Black;
+            this.picBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picBox.Location = new System.Drawing.Point(0, 0);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(631, 327);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox.TabIndex = 2;
+            this.picBox.TabStop = false;
+            // 
+            // resizeButton
+            // 
+            this.resizeButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.resizeButton.Location = new System.Drawing.Point(207, 333);
+            this.resizeButton.Name = "resizeButton";
+            this.resizeButton.Size = new System.Drawing.Size(68, 25);
+            this.resizeButton.TabIndex = 3;
+            this.resizeButton.Text = "Actual Size";
+            this.resizeButton.UseVisualStyleBackColor = true;
+            this.resizeButton.Click += new System.EventHandler(this.resizeButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.saveButton.Location = new System.Drawing.Point(355, 333);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(68, 25);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save As...";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // picPanel
+            // 
+            this.picPanel.AutoScroll = true;
+            this.picPanel.Controls.Add(this.picBox);
+            this.picPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.picPanel.Location = new System.Drawing.Point(0, 0);
+            this.picPanel.Name = "picPanel";
+            this.picPanel.Size = new System.Drawing.Size(631, 327);
+            this.picPanel.TabIndex = 5;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(631, 361);
+            this.Controls.Add(this.picPanel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.resizeButton);
+            this.Controls.Add(this.openButton);
+            this.MinimumSize = new System.Drawing.Size(240, 82);
+            this.Name = "mainForm";
+            this.Text = "Glimpse";
+            this.Resize += new System.EventHandler(this.Window_Resized);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.picPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Button resizeButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Panel picPanel;
+    }
+}
+
---
+++ Glimpse/MainForm.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace Glimpse
+{
+    public partial class mainForm : Form
+    {
+        bool isZoomed = true;
+        string fileName = "";
+        int extIndex = 1;
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+        private void Window_Resized(object sender, EventArgs e)
+        {
+            picPanel.Width = this.Width - 16;
+            picPanel.Height = this.Height - 73;
+            SetZoom(isZoomed);
+        }
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp;*.tif;*.tiff";
+            openFile.Title = "Select an Image File";
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                FileStream openStream = (FileStream)openFile.OpenFile();
+                picBox.Image = Image.FromStream(openStream);
+                openStream.Dispose();
+                fileName = Path.GetFileNameWithoutExtension(openFile.SafeFileName);
+                extIndex = Ext_to_Index(Path.GetExtension(openFile.SafeFileName).ToLower());
+            }
+        }
+        private void resizeButton_Click(object sender, EventArgs e)
+        {
+            if (isZoomed == true)
+            {
+                resizeButton.Text = "Fit Window";
+                isZoomed = false;
+            }
+            else if (resizeButton.Text == "Fit Window")
+            {
+                resizeButton.Text = "Actual Size";
+                isZoomed = true;
+            }
+            SetZoom(isZoomed);
+        }
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "*JPEG Image|*.jpg|PNG Image|*.png|GIF Image|*.gif|Bitmap Image|*.bmp|TIFF Image|*.tif";
+            saveFile.FilterIndex = extIndex;
+            saveFile.FileName = fileName;
+            saveFile.AddExtension = true;
+            saveFile.Title = "Save Image As...";
+            if (saveFile.ShowDialog() == DialogResult.OK && saveFile.FileName != "")
+            {
+                FileStream saveStream = (FileStream)saveFile.OpenFile();
+                switch(saveFile.FilterIndex)
+                {
+                    case 1:
+                        picBox.Image.Save(saveStream, ImageFormat.Jpeg);
+                        break;
+                    case 2:
+                        picBox.Image.Save(saveStream, ImageFormat.Png);
+                        break;
+                    case 3:
+                        picBox.Image.Save(saveStream, ImageFormat.Gif);
+                        break;
+                    case 4:
+                        picBox.Image.Save(saveStream, ImageFormat.Bmp);
+                        break;
+                    case 5:
+                        picBox.Image.Save(saveStream, ImageFormat.Tiff);
+                        break;
+                }
+                saveStream.Dispose();
+                fileName = saveFile.FileName;
+            }
+        }
+        private void SetZoom(bool areZoomed)
+        {
+            if (areZoomed == true)
+            {
+                picBox.Dock = DockStyle.Fill;
+                picBox.Size = picPanel.Size;
+                picBox.SizeMode = PictureBoxSizeMode.Zoom;                
+            }
+            else
+            {
+                if (picBox.Image.Height > picPanel.Height)
+                {
+                    picBox.Dock = DockStyle.None;
+                    if (picBox.Image.Width > picPanel.Width)
+                    {                        
+                        picBox.Size = picBox.Image.Size;
+                    }
+                    else
+                    {
+                        picBox.Height = picBox.Image.Height;
+                        picBox.Width = picPanel.Width - 17;
+                    }
+                }
+                else
+                {
+                    if (picBox.Image.Width > picPanel.Width)
+                    {
+                        picBox.Dock = DockStyle.None;
+                        picBox.Height = picPanel.Height - 17;
+                        picBox.Width = picBox.Image.Width;
+                    }
+                    else
+                    {
+                        picBox.Dock = DockStyle.Fill;
+                    }
+                }
+                picBox.SizeMode = PictureBoxSizeMode.CenterImage;
+            }
+        }
+        private int Ext_to_Index(string fileExt)
+        {
+            int newIndex = 1;
+            switch (fileExt)
+            {
+                case ".jpg":
+                    newIndex = 1;
+                    break;
+                case ".jpeg":
+                    newIndex = 1;
+                    break;
+                case ".png":
+                    newIndex = 2;
+                    break;
+                case ".gif":
+                    newIndex = 3;
+                    break;
+                case ".bmp":
+                    newIndex = 4;
+                    break;
+                case ".tif":
+                    newIndex = 5;
+                    break;
+                case ".tiff":
+                    newIndex = 5;
+                    break;
+            }
+            return newIndex;
+        }
+    }
+}
